cc9d950d7ebf17744b0a24a1c9eb831c4dce8920
tour1st <sosnovskyivsosnovsky@yandex.ru>
2024-10-01 13:01:17 +0300

    + класс vshell с конструктором и каркас для функции загрузки файловой системы

--- homework1/main.py
+++ homework1/main.py
@@ -1,1 +1,14 @@
+import zipfile
 
+class VShell:
+    def __init__(self, archive_path, logfile):
+        self.current_dir = "/"
+        self.previous_dir = "/"
+        self.archive_path = archive_path
+        self.file_system = self.load_archive(archive_path)
+
+    def load_archive(self, path):
+        if path.endswith(".zip"):
+            pass
+        else:
+            raise ValueError("Unsupported archive format")
