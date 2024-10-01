fa37f3351bde54db5a8ad4fbcc242c9ea19a6f0b
tour1st <sosnovskyivsosnovsky@yandex.ru>
2024-10-01 13:28:42 +0300

    + функция запуска программы, задаток для реализации и тестирования команд vshell

--- homework1/main.py
+++ homework1/main.py
@@ -1,4 +1,5 @@
 import zipfile
+import argparse
 
 class VShell:
     def __init__(self, archive_path):
@@ -13,3 +14,27 @@ class VShell:
                 return ["/".join(file.split("/")[1:]) for file in zip.namelist() if "/".join(file.split("/")[1:]) != ""]
         else:
             raise ValueError("Unsupported archive format")
+        
+    def run_command(self, command):
+        pass
+        
+
+def main():
+    parser = argparse.ArgumentParser(description="Virtual Shell")
+    parser.add_argument("user", help="User's name")
+    parser.add_argument("pcname", help="PC's name")
+    parser.add_argument("archive", help="archive path")
+    args = parser.parse_args()
+
+    shell = VShell(args.archive)
+
+    while True:
+        try:
+            command = input(f"{args.user}@{args.pcname}:{shell.current_dir}$ ")
+            shell.run_command(command)
+        except EOFError:
+            break
+
+
+if __name__ == "__main__":
+    main()
