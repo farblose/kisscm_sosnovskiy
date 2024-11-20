ffc49058932d6bf346931f48170b41761676d5f0
xor_rax_rax <xor_rax_rax@thesecure.biz>
2024-11-20 16:59:29 +0300

    + PackedObject implementation

---
+++ homework2/PackedObject.hpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <cstdint>
+#include <string>
+
+#ifndef PACKEDOBJECT_HPP
+#define PACKETOBJECT_HPP
+
+enum class GitObjectType {
+    COMMIT = 1,
+    TREE = 2,
+    BLOB = 3,
+    TAG = 4,
+    OFS_DELTA = 6,
+    REF_DELTA = 7
+};
+
+class PackedObject {
+public:
+    GitObjectType type;
+    size_t size;
+    std::vector<uint8_t> data;
+    uint64_t baseOffset;  // Для OFS_DELTA
+    std::string baseHash; // Для REF_DELTA
+};
+
+#endif
